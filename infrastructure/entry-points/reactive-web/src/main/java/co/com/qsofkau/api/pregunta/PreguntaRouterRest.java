e77d07c38dbbf2f4a67245da6b55795f12b109dc
juan9507 <juandavidnaranjo75@gmail.com>
2022-08-06 18:08:35 -0500

    creacion etry points para las preguntas

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/qsofkau/api/pregunta/PreguntaRouterRest.java
@@ -0,0 +1,21 @@
+package co.com.qsofkau.api.pregunta;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class PreguntaRouterRest {
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(PreguntaHandler preguntaHandler){
+        return route(POST("api/pregunta/guardar"), preguntaHandler::POSTGuardarPreguntaUseCase)
+                .and(route(GET("api/pregunta/listar"), preguntaHandler::GETListarPreguntasUseCase))
+                .and(route(GET("api/pregunta/listar/{id}"), preguntaHandler::GETListarPreguntaConIdUseCase))
+                .and(route(PUT("api/pregunta/actualizar/{id}"), preguntaHandler::PUTActualizarPreguntaUseCase));
+    }
+}
