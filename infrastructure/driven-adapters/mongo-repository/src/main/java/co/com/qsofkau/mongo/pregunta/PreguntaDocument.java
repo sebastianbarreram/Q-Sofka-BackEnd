5c465aa4763dfb5ce1d16480e6800863d2172540
juan9507 <juandavidnaranjo75@gmail.com>
2022-08-06 17:26:54 -0500

    creacion en infrastructure los driven adapters para pregunta

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/qsofkau/mongo/pregunta/PreguntaDocument.java
@@ -0,0 +1,22 @@
+package co.com.qsofkau.mongo.pregunta;
+
+import co.com.qsofkau.model.pregunta.Opcion;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@Document
+@Data
+public class PreguntaDocument {
+
+    @Id
+    private String id;
+    private String coachId;
+    private String fecha;
+    private String pregunta;
+    private String areaConocimiento;
+    private String tipoPregunta;
+    private Set<Opcion> opciones;
+}
