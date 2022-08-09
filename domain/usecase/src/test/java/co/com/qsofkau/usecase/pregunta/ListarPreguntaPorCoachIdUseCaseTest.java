d39073add5b54d5702efb8e40a1319854c608fce
duvanleal96 <duvanleal65@gmail.com>
2022-08-09 15:30:01 -0500

    test listar preguntas por coach id

--- domain/usecase/src/test/java/co/com/qsofkau/usecase/pregunta/ListarPreguntaPorCoachIdUseCaseTest.java
+++ domain/usecase/src/test/java/co/com/qsofkau/usecase/pregunta/ListarPreguntaPorCoachIdUseCaseTest.java
@@ -7,29 +7,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.time.LocalDateTime;
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+
 @ExtendWith(MockitoExtension.class)
-class ListarPreguntaConIdUseCaseTest {
+
+public class ListarPreguntaPorCoachIdUseCaseTest {
 
     @InjectMocks
-    ListarPreguntaConIdUseCase useCase;
+    ListarPreguntaPorCoachIdUseCase useCase;
 
     @Mock
     PreguntaRepository repository;
 
     @Test
-    public void listar_pregunta_con_id_test(){
-
+    public void listar_pregunta_por_coachid_exitoso(){
+        //arrange
         Opcion opcion1 = new Opcion("verdadero", true);
         Opcion opcion2 = new Opcion("falso", false);
         Set<Opcion> opciones = Set.of(opcion1, opcion2);
@@ -44,12 +43,13 @@ class ListarPreguntaConIdUseCaseTest {
                 "tipo pregunta",
                 opciones
         );
+        when(repository.findByCoachId("c1")).thenReturn(Flux.just(pregunta));
 
-        when(repository.findById(Mockito.any(String.class))).thenReturn(Mono.just(pregunta));
-
-        StepVerifier.create(useCase.listarPreguntaConId(pregunta.getId()))
+        StepVerifier.create(useCase.listarPorCoachId(pregunta.getCoachId()))
                 .expectNext(pregunta)
                 .expectComplete()
                 .verify();
     }
+
+
 }
