673954106386ad2d2f84833e67253c75628dfffd
duvanleal96 <duvanleal65@gmail.com>
2022-08-10 16:46:37 -0500

    prueba unitaria encontarAspirantePorCodigo

--- domain/usecase/src/test/java/co/com/qsofkau/usecase/aspirante/encontrarAspirantePorCodigo/EncontrarAspirantePorCodigoUseCaseTest.java
+++ domain/usecase/src/test/java/co/com/qsofkau/usecase/aspirante/encontrarAspirantePorCodigo/EncontrarAspirantePorCodigoUseCaseTest.java
@@ -1,14 +1,22 @@
 package co.com.qsofkau.usecase.aspirante.encontrarAspirantePorCodigo;
 
+import co.com.qsofkau.model.aspirante.Aspirante;
 import co.com.qsofkau.model.aspirante.gateways.AspiranteRepository;
+import co.com.qsofkau.model.usuario.Usuario;
 import co.com.qsofkau.usecase.aspirante.generarCodigo.GenerarCodigoUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class EncontrarAspirantePorCodigoUseCaseTest {
     @InjectMocks
@@ -20,6 +28,15 @@ class EncontrarAspirantePorCodigoUseCaseTest {
 
     @Test
     void encontrarAspirantePorCodigo() {
+        Aspirante aspirante = new Aspirante("1","juan","duvanleal65@gmail.com",1,"aprobo","555-hhh");
+        Aspirante aspirante1 = new Aspirante("2","duvan","duvanleal96@gmail.com",1,"aprobo","552-xxx");
+        List<Aspirante> aspirantes = List.of(aspirante, aspirante1);
 
+        when(repository.findAll()).thenReturn(Flux.fromIterable(aspirantes));
+
+        StepVerifier.create(useCase.encontrarAspirantePorCodigo(aspirante1.getCodigoVerificacion()))
+                .expectNext(aspirante1)
+                .expectComplete()
+                .verify();
     }
 }
