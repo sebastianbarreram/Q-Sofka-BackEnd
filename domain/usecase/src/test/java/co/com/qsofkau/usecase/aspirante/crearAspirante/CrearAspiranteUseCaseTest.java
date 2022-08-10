a86e03ecce14626e924651a1f69c49f54bc19688
duvanleal96 <duvanleal65@gmail.com>
2022-08-10 11:42:48 -0500

    test H4
    se agregan los test historia de usuario 4

---
+++ domain/usecase/src/test/java/co/com/qsofkau/usecase/aspirante/crearAspirante/CrearAspiranteUseCaseTest.java
@@ -0,0 +1,12 @@
+package co.com.qsofkau.usecase.aspirante.crearAspirante;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrearAspiranteUseCaseTest {
+
+    @Test
+    void crearAspirante() {
+    }
+}
