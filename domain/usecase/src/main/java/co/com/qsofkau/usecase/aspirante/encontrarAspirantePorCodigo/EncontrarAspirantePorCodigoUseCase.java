f1106fffebdffc80c033ff00fe906bcc63d62b92
felipemarin93 <felipemaringiraldo@gmail.com>
2022-08-11 15:05:03 -0500

    Corrección Bug Caso de Uso EncontrarAspirantePorCódigo en en el backend para la HU#4

--- domain/usecase/src/main/java/co/com/qsofkau/usecase/aspirante/encontrarAspirantePorCodigo/EncontrarAspirantePorCodigoUseCase.java
+++ domain/usecase/src/main/java/co/com/qsofkau/usecase/aspirante/encontrarAspirantePorCodigo/EncontrarAspirantePorCodigoUseCase.java
@@ -5,6 +5,8 @@ import reactor.core.publisher.Mono;
 import co.com.qsofkau.model.aspirante.gateways.AspiranteRepository;
 import co.com.qsofkau.model.aspirante.Aspirante;
 
+import java.util.Objects;
+
 @RequiredArgsConstructor
 public class EncontrarAspirantePorCodigoUseCase {
 
@@ -12,6 +14,7 @@ public class EncontrarAspirantePorCodigoUseCase {
 
     public Mono<Aspirante> encontrarAspirantePorCodigo(String codigo){
         return aspiranteRepository.findAll()
+                .filter(aspirante -> Objects.nonNull(aspirante.getCodigoVerificacion()))
                 .filter(aspirante -> aspirante.getCodigoVerificacion().equals(codigo)).next();
     }
 }
