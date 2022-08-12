ef3fa15720b661e356ab8b1b5281e526dcb254c9
miller gallego <miller.gallegof@gmail.com>
2022-08-11 19:01:49 -0500

    Arreglo cors

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/qsofkau/api/config/CorsGlobalConfiguration.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/qsofkau/api/config/CorsGlobalConfiguration.java
@@ -1,18 +1,23 @@
 package co.com.qsofkau.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
-@EnableWebFlux
-public class CorsGlobalConfiguration implements WebFluxConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200/", "*") //TODO AGREGAR ORIGEN DESPLEGADO
-                .allowedMethods("GET","POST","PUT", "DELETE")
-                .allowCredentials(true).maxAge(3600);
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public WebFluxConfigurer corsConfigure() {
+        return new WebFluxConfigurerComposite() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
